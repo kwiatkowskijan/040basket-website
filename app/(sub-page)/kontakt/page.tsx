74993b338fb7113959b471156e7de5e4572de435
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2026-02-21 16:36:28 +0100

    fix(ContactPage): responsive layout

--- app/(sub-page)/kontakt/page.tsx
+++ app/(sub-page)/kontakt/page.tsx
@@ -16,28 +16,30 @@ export default function Contact() {
     <main>
       <BackgroundLogo />
       <PageTitle />
-      <section className="w-full container mx-auto flex flex-row justify-center items-center text-center min-h-100">
-        <div className="w-1/2 flex align-top justify-start">
-          <div className="w-1/3 flex justify-end border-r-4 border-primary pr-8">
-            <Image src="/logo.svg" alt="Logo" width={200} height={100} />
+      <section className="w-full container mx-auto min-h-100">
+        <div className="w-full flex flex-col px-2 md:flex-row justify-center items-center gap-16">
+          <div className="w-full md:w-1/2 flex align-top justify-start">
+            <div className="w-1/3 flex justify-end border-r-4 border-primary pr-8">
+              <Image src="/logo.svg" alt="Logo" width={200} height={100} />
+            </div>
+            <div className="w-full md:w-2/3 text-left pl-8">
+              <p className="text-md md:text-2xl">
+                Stowarzyszenie 040Basket
+                <br />
+                ul. Przykładowa 12
+                <br />
+                00-000 Miasto
+                <br />
+                email: kontakt@040basket.pl
+                <br />
+                tel: +48 123 456 789
+              </p>
+            </div>
           </div>
-          <div className="w-2/3 text-left pl-8">
-            <p className="text-2xl">
-              Stowarzyszenie 040Basket
-              <br />
-              ul. Przykładowa 12
-              <br />
-              00-000 Miasto
-              <br />
-              email: kontakt@040basket.pl
-              <br />
-              tel: +48 123 456 789
-            </p>
-          </div>
-        </div>
-        <div className="w-1/2 h-full">
-          <div className="bg-primary p-8">
-            <ContactForm />
+          <div className="w-full md:w-1/2 h-full">
+            <div className="bg-primary p-8">
+              <ContactForm />
+            </div>
           </div>
         </div>
       </section>
