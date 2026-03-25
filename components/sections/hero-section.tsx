b99ae42440e336aed4d53c8606e31de5a9cee23b
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2026-03-25 19:19:43 +0100

    feat(HeroSection): implemented hero section

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Calendar, Handshake, MapPin, Users } from "lucide-react";
 
 export default function HeroSection() {
   return (
@@ -6,14 +7,35 @@ export default function HeroSection() {
       <video className="w-full h-full object-cover" autoPlay muted loop>
         <source src="/040_v2.mp4" type="video/mp4" />
       </video>
-      <div className="absolute inset-0 bg-black/40"></div>
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
+      <div className="absolute inset-0 bg-black/65"></div>
+      <div className="absolute inset-0 flex flex-col items-center justify-center gap-8 text-center px-4">
+        <p className="text-primary uppercase font-bold tracking-widest">
+          Turniej koszykówki 3x3
+        </p>
         <h1 className="text-4xl md:text-6xl lg:text-9xl text-white mb-4 font-heading ">
-          Still Ballin
+          040Basket <br />
+          <span className="font-heading text-primary">Tournament 4.0</span>
         </h1>
-        <Button variant={"default"} size={"lg"}>
-          Dołącz do nas
-        </Button>
+        <div className="flex items-center gap-6 text-white">
+          <div className="flex items-center gap-2">
+            <Calendar />
+            15-16 lipca 2026
+          </div>
+          <div className="flex items-center gap-2">
+            <MapPin />
+            Puszczykowo, 62-040
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <Button variant={"default"} size={"lg"}>
+            <Users />
+            Zapisz swoją drużynę
+          </Button>
+          <Button variant={"outline"} size={"lg"}>
+            <Handshake />
+            Zostań partnerem
+          </Button>
+        </div>
       </div>
     </div>
   );
